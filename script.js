d8c7200d1730e377923ea574d46e70bde40fb40c
iabad1 <63217881+iabad1@users.noreply.github.com>
2021-09-12 19:58:35 -0700

    Nav bar completed

---
+++ script.js
@@ -0,0 +1,8 @@
+//Isiah Abad
+//Last Updated: 9/12/21
+
+//Description: Opening and close the hamburger menu script
+let open = false;
+document.getElementById("hamburger-menu-container").addEventListener("click", ()=>{
+    
+});

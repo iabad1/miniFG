24c4b287ecd835b72fe1155fb7cd7717c5b55747
iabad1 <63217881+iabad1@users.noreply.github.com>
2021-09-13 01:12:17 -0700

    JS added for menu bar

--- script.js
+++ script.js
@@ -2,7 +2,19 @@
 //Last Updated: 9/12/21
 
 //Description: Opening and close the hamburger menu script
-let open = false;
+//let open = false;
+let navListener = document.getElementById("nav-links");
 document.getElementById("hamburger-menu-container").addEventListener("click", ()=>{
     
+    //add open or close class
+    if(navListener.classList[0] === "close"){//opening the nav
+        console.log("it is closed");
+        navListener.classList.remove('close');
+        navListener.classList.add('open');
+    }
+    else{
+        console.log("it is open");
+        navListener.classList.remove('open');
+        navListener.classList.add('close');
+    }
 });

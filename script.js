b8565d7ac110f06579714b53b195b72a4f3a43bb
iabad1 <63217881+iabad1@users.noreply.github.com>
2021-09-13 12:56:36 -0700

    Added open nav feature and centered nav links

--- script.js
+++ script.js
@@ -3,18 +3,22 @@
 
 //Description: Opening and close the hamburger menu script
 //let open = false;
-let navListener = document.getElementById("nav-links");
-document.getElementById("hamburger-menu-container").addEventListener("click", ()=>{
+let navListener = document.getElementById('nav-links');
+let lineListener = document.querySelectorAll(".line");
+document.getElementById('hamburger-menu-container').addEventListener('click', ()=>{
     
     //add open or close class
-    if(navListener.classList[0] === "close"){//opening the nav
+    if(navListener.classList[0] === 'close'){//opening the nav
         console.log("it is closed");
         navListener.classList.remove('close');
         navListener.classList.add('open');
+        lineListener.forEach(el => el.classList.add('open-line'));
     }
     else{
         console.log("it is open");
         navListener.classList.remove('open');
         navListener.classList.add('close');
+        lineListener.forEach(el => el.classList.remove('open-line'));
+        
     }
 });

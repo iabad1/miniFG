050600a66f5018923674ec5d20da0ee2c1c4a855
iabad1 <63217881+iabad1@users.noreply.github.com>
2021-09-16 15:47:37 -0700

    Finished the Games information and contact page

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
 //Description: Opening and close the hamburger menu script
 //let open = false;
 let navListener = document.getElementById('nav-links');
+let bodyListener = document.getElementById('main-body-container');
 let lineListener = document.querySelectorAll(".line");
 document.getElementById('hamburger-menu-container').addEventListener('click', ()=>{
     
@@ -12,13 +13,27 @@ document.getElementById('hamburger-menu-container').addEventListener('click', ()
         console.log("it is closed");
         navListener.classList.remove('close');
         navListener.classList.add('open');
+        bodyListener.classList.add('open');
         lineListener.forEach(el => el.classList.add('open-line'));
     }
     else{
         console.log("it is open");
         navListener.classList.remove('open');
         navListener.classList.add('close');
+        bodyListener.classList.remove('open');
+
         lineListener.forEach(el => el.classList.remove('open-line'));
         
+    }
+});
+
+window.addEventListener('resize', ()=>{
+    if(window.innerWidth >= 768 ){
+        console.log("Width is at 768px");
+        navListener.classList.remove('open');
+        navListener.classList.add('close');
+        bodyListener.classList.remove('open');
+
+        lineListener.forEach(el => el.classList.remove('open-line'));
     }
 });
